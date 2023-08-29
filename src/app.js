b3bb63955d479cccf850f722be3ea1d5dcff2d43
Aryamanraj <20cs3011@rgipt.ac.in>
2023-08-30 00:27:34 +0530

    main app and api swagger docs

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('src/swagger.yaml');
+
+const certificatesRouter = require('./api/routes/certificates');
+const errorHandler = require('./api/middlewares/errorHandler');
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', certificatesRouter); // Prefix all routes with '/api'
+app.use(errorHandler); // This should be after your routes
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
